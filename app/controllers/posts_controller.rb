abaf5fae4cca2ffaca64b8bc6a7ee57479c62cf6
Abu Butt <buttabu@yahoo.com>
2017-06-18 17:26:04 -0400

    Models and Database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
 	def index
+		@content_first = "This is some sample text for My Ruby Blog";
+		@content_second = "This is additional sample text for My Ruby Blog";
+	end
 
+	def new
+	
+	end
+
+	def create
+	
+	end
+
+	def edit
+	
+	end
+
+	def update
+	
+	end
+
+	def show
+	
+	end
+
+	def destroy
+	
 	end
 end
