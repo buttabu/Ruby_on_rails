8e82c150375a5eafeb80d15355b8b80f7b864227
Abu Butt <buttabu@yahoo.com>
2017-06-23 17:47:48 -0400

    More integrating css

--- Blog/app/controllers/application_controller.rb
+++ Blog/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :all_categories
+  helper_method :all_post
+
+  def all_categories
+  	@categories = Category.all
+  end
+
+  def all_post
+  	@post = Post.all
+  end
 end
